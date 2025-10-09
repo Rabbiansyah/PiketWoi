8bac4fb26478ec7e1cc957be7162c25f19d9e2bf
rafif <rafifdzakyakmal@gmail.com>
2025-10-09 22:16:57 +0700

    update minor 4.6.1

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::get('/welcome', function () {
+Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
